0b602195bccaeca2fc67221e1d3771a1805bc5f0
VittleSchnitzel <rfs.4ent@gmail.com>
2026-01-17 11:49:01 -0800

    Rename the utility classes to reflect that they are 3d. Code Vector3, Triangle3.

---
+++ src/util/Triangle3.java
@@ -0,0 +1,17 @@
+package util;
+
+import java.awt.*;
+
+public class Triangle3 {
+    Vector3 a;
+    Vector3 b;
+    Vector3 c;
+    Color color;
+
+    public Triangle3(Vector3 a, Vector3 b, Vector3 c, Color color) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.color = color;
+    }
+}
